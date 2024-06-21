1c05ce96dda656d9a28c24ba19c3187ad24e4a5b
Victor <nicolas.cabezas@udla.edu.ec>
2024-06-20 22:52:34 -0500

    Victor

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,8 +9,15 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+                    {{ __("BienVenudo!") }}
                 </div>
+                @foreach ($users as $user)
+                    <div class="p-6 text-gray-900 dark:text-gray-100">
+                        <p>{{ $user->name }}</p>
+                        <p>{{ $user->email }}</p>
+                    </div>
+                    
+                @endforeach
             </div>
         </div>
     </div>
